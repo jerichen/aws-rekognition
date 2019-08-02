fc9bdf89ddf9025ff6b348bb8b044f0cb4af560d
JeriChen <jeri.chen0110@gmail.com>
2019-08-02 16:43:08 +0800

    save new excel

--- api.py
+++ api.py
@@ -11,22 +11,38 @@ if __name__ == "__main__":
     print('表單名稱：', excel_data.sheet_names())
     print('表單 %s 共 %d 行 %d 列' % (sheet_0.name, sheet_0.nrows, sheet_0.ncols))
 
+    # 寫入新的excel title
+    new_sheet.write(0, 0, 'webid')
+    new_sheet.write(0, 1, 'Image No.')
+    new_sheet.write(0, 2, '圖片名稱')
+    new_sheet.write(0, 3, 'labes')
+
     for i in range(1, sheet_0.nrows):
         IMAGE_FILE = os.path.join(os.getenv('IMAGES_PATH'), sheet_0.cell_value(i, 0) + '.jpg')
         print('--------------------------------------------------')
-        print('第 %s 行第 1 列: %s' % (i, sheet_0.cell_value(i, 0)))
         print('圖片名稱 : ' + IMAGE_FILE)
 
-        lists = ['Nature', 'Outdoors', 'Countryside', 'Hill', 'Slope', 'Mountain', 'Plateau', 'Mountain', 'Range']
+        new_sheet.write(i, 0, sheet_0.cell_value(i, 0))
+        new_sheet.write(i, 1, sheet_0.cell_value(i, 1))
+        new_sheet.write(i, 2, IMAGE_FILE)
 
+        labels = detect_labels(IMAGE_FILE)
+        if type(labels) is list:
+            j = 2
+            for label in labels:
+                j += 1
+                new_sheet.write(i, j, label['Name'])
+        else:
+            print('call api fail')
 
-        # labels = detect_labels(IMAGE_FILE)
-        # if type(labels) is list:
-        #     for label in labels:
-        #         print(label['Name'])
-        # else:
-        #     print('call api fail')
+        # labels = ['Nature', 'Outdoors', 'Countryside', 'Hill', 'Slope', 'Mountain', 'Plateau', 'Mountain', 'Range']
+        # j = 2
+        # for label in lists:
+        #     j += 1
+        #     new_sheet.write(i, j, label)
 
+        print('--------------------------------------------------')
         break
 
-    # print('Done...')
+    new_excel.save('images-labels.xls')
+    print('Done..............')
--- modules/read_excel.py
+++ modules/read_excel.py
@@ -11,3 +11,9 @@ row_data = excel_data.sheets()[0]
 
 # 取得第一個表單
 sheet_0 = excel_data.sheet_by_index(0)
+
+# 創建 xlsx 表單
+new_excel = xlwt.Workbook()
+
+# 新建一個表單
+new_sheet = new_excel.add_sheet('sheet1')
